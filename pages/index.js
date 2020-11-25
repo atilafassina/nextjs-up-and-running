59cd99dc307dbd01d0490a32941594149e4b4ea6
Atila Fassina <atila@fassina.eu>
2020-11-25 13:46:18 +0100

    lesson 1

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -10,9 +10,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Next Website 🔥</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -61,5 +59,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
