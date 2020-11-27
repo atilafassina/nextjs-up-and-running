a5e856fb86a08526f6e5c752823978b2385785af
Atila Fassina <atila@fassina.eu>
2020-11-27 09:47:40 +0100

    lesson 7

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import Navigation from '@components/navigation'
+
+export default function About () {
+  return (
+    <>
+      <Navigation />
+      <header>
+        <h2>Hello from about page!</h2>
+      </header>
+    </>
+  )
+}
