837daa92783eb68237daeb1e03fa3a2b67703cc8
Atila Fassina <atila@fassina.eu>
2020-11-27 11:48:19 +0100

    lesson 9 and 10

--- pages/post/hello-there.tsx
+++ pages/post/hello-there.tsx
@@ -1,7 +1,22 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
 const Hello = () => (
-  <div>
-    <h1>Hello There!</h1>
-  </div>
+  <>
+    <Head>
+      <title>Hello there :: My Blog</title>
+      <meta property="og:title" content="Hello there :: My Blog" />
+    </Head>
+    <article>
+      <h1>Hello There!</h1>
+      <Image
+        src="/post/hello-there/jcemer-road-ahead.jpg"
+        width={640}
+        height={427}
+      />
+      <p>Photo by <a href="https://unsplash.com/@jeancarloemer">Jean Carlo Emer</a> on Unsplash</p>
+    </article>
+  </>
 )
 
 export default Hello
